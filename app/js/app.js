d4e91680b9a77d52e584fc397e84a8d6bee1ccc6
Evgeny <260443@mail.ru>
2024-03-06 21:31:14 +0500

    4 main section

--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,23 @@
 // import PhotoSwipeLightbox from 'photoswipe/lightbox';
 // import IMask from 'imask';
 
+import ImageCompare from "image-compare-viewer";
 
+
+// Image compare
+// https://image-compare-viewer.netlify.app/
+// https://github.com/kylewetton/image-compare-viewer
+const viewers = document.querySelectorAll(".image-compare");
+  
+viewers.forEach((element) => {
+  let view = new ImageCompare(element).mount();
+});
+
+
+
+
+// Footer year
+let footerCurrentYear = document.querySelector('.footer-current-year');
+const now = new Date();
+
+footerCurrentYear.innerText = now.getFullYear();
