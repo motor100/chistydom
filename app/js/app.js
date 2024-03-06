e225868098defc8e33ee6a98241cf44f7945488f
Evgeny <260443@mail.ru>
2024-03-06 18:51:19 +0500

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+// import Swiper, { Navigation } from 'swiper';
+// import AirDatepicker from 'air-datepicker';
+// import PhotoSwipeLightbox from 'photoswipe/lightbox';
+// import IMask from 'imask';
+
+
